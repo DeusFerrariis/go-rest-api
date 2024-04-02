bea77325931bc5b6fafcb31d3dff04db55b6ffaa
Will Cleghorn <63998977+DeusFerrariis@users.noreply.github.com>
2024-04-02 01:31:32 +0000

    Replaces string context keys w wrapped strings for collision avoidance, added handler funcs for new post.

--- user.go
+++ user.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/charmbracelet/log"
+	"github.com/go-chi/chi/v5"
 )
 
 type User struct {
@@ -14,16 +15,28 @@ type User struct {
 }
 
 type Post struct {
-	author  string
-	content string
+	Author  string `json:"author"`
+	Content string `json:"content"`
 }
 
 type Middleware func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc)
 
+type ContextKey struct {
+	string
+}
+
 func WithUsers(users []User) func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	return func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		ctx := r.Context()
-		ctx = context.WithValue(ctx, "users", &users)
+		ctx = context.WithValue(ctx, ContextKey{"users"}, &users)
+		next(rw, r.WithContext(ctx))
+	}
+}
+
+func WithPosts(posts []Post) func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	return func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, ContextKey{"posts"}, &posts)
 		next(rw, r.WithContext(ctx))
 	}
 }
@@ -33,7 +46,7 @@ type CreateUserRequest struct {
 }
 
 func HandleCreateUser(rw http.ResponseWriter, r *http.Request) {
-	users, ok := r.Context().Value("users").(*[]User)
+	users, ok := r.Context().Value(ContextKey{"users"}).(*[]User)
 	log.Debug("users", users)
 	if !ok {
 		rw.WriteHeader(http.StatusInternalServerError)
@@ -59,4 +72,53 @@ func HandleCreateUser(rw http.ResponseWriter, r *http.Request) {
 
 	*users = append(*users, User{Name: userReq.Username})
 	rw.WriteHeader(http.StatusAccepted)
+}
+
+type CreatePostRequest struct {
+	Username string `json:"username"`
+	Content  string `json:"content"`
+}
+
+func HandleCreatePost(rw http.ResponseWriter, r *http.Request) {
+	var postReq CreatePostRequest
+	if err := json.NewDecoder(r.Body).Decode(&postReq); err != nil {
+		log.Error("recieved bad request", "err", err)
+		rw.WriteHeader(http.StatusBadRequest)
+		io.WriteString(rw, "invalid payload")
+		return
+	}
+
+	p := Post{
+		Author:  postReq.Username,
+		Content: postReq.Content,
+	}
+
+	posts, ok := r.Context().Value(ContextKey{"posts"}).(*[]Post)
+	log.Debug("posts", posts)
+	if !ok {
+		rw.WriteHeader(http.StatusInternalServerError)
+		log.Error("could not retrieve posts from request context")
+		return
+	}
+
+	*posts = append(*posts, p)
+}
+
+// func HandleCreatePost(rw http.ResponseWriter, r *http.Request) {
+func HandleGetUser(rw http.ResponseWriter, r *http.Request) {
+	if name := chi.URLParam(r, "username"); name == "" {
+		rw.WriteHeader(http.StatusBadRequest)
+		io.WriteString(rw, "missing name parameter in URL query")
+		return
+	}
+
+	
+}
+
+func HandleGetUserPosts(rw http.ResponseWriter, r *http.Request) {
+	if name := chi.URLParam(r, "username"); name == "" {
+		rw.WriteHeader(http.StatusBadRequest)
+		io.WriteString(rw, "missing name parameter in URL query")
+		return
+	}
 }
